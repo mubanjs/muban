34e8cb160e85601ce02ee9fd0ee8f594607704eb
Lars van Braam <lars@mediamonks.com>
2020-12-02 12:41:53 +0100

    Add the cf-a1-heading atom

--- examples/src/components/atom/cf-a1-heading/CfA1Heading.stories.ts
+++ examples/src/components/atom/cf-a1-heading/CfA1Heading.stories.ts
@@ -1,45 +1,73 @@
+import { cfA1Heading } from './CfA1Heading';
 import {
-  cfA1Heading,
-  defaultHeadingAlignment, defaultHeadingClass,
+  defaultHeadingAlignment,
+  defaultHeadingClass,
   defaultHeadingType,
-  headingAlignments, headingClasses,
+  headingAlignments,
+  headingClasses,
   headingTypes,
-} from './CfA1Heading';
+} from './CfA1Heading.config';
+import type { CFa1HeadingProps } from './CfA1Heading.types';
+import type { Story } from '@muban/storybook/dist/client/preview/types-6-0';
 
 export default {
   title: 'Atom/cf-a1-heading',
   argTypes: {
     eyebrow: {
       control: 'text',
-      defaultValue: 'The quick brown fox jumps over the lazy dog.',
+      description: 'The text that is rendered above the main title (can contain HTML)',
+      type: {
+        required: false,
+      },
       table: {
         category: 'Data',
+        type: {
+          summary: 'string',
+        },
       },
     },
     title: {
       control: 'text',
+      description: 'The main title that is rendered (can contain HTML).',
       defaultValue: 'The quick brown fox jumps over the lazy dog.',
+      type: {
+        required: true,
+      },
       table: {
         category: 'Data',
+        type: {
+          summary: 'string',
+        },
       },
     },
     mustache: {
       control: 'text',
-      defaultValue: 'The quick brown fox jumps over the lazy dog.',
+      description: 'The text that is rendered below the main title (can contain HTML)',
+      type: {
+        required: false,
+      },
       table: {
         category: 'Data',
+        type: {
+          summary: 'string',
+        },
       },
     },
     alignment: {
-      description: 'The alignment of the heading within the container',
+      description: 'The alignment of the heading within the container.',
       defaultValue: defaultHeadingAlignment,
+      type: {
+        required: false,
+      },
       control: {
         type: 'select',
         options: headingAlignments,
       },
       table: {
         category: 'Visual',
-        type: 'select',
+        type: {
+          summary: headingAlignments.join('|'),
+        },
         defaultValue: {
           summary: defaultHeadingAlignment,
         },
@@ -48,13 +76,18 @@ export default {
     headingClass: {
       description: 'The heading class that is applied on the component.',
       defaultValue: defaultHeadingClass,
+      type: {
+        required: false,
+      },
       control: {
         type: 'select',
         options: headingClasses,
       },
       table: {
         category: 'Visual',
-        type: 'select',
+        type: {
+          summary: headingClasses.join(' | '),
+        },
         defaultValue: {
           summary: defaultHeadingClass,
         },
@@ -62,16 +95,24 @@ export default {
     },
     className: {
       description: 'Any custom css classes that you might want to add to the component.',
+      type: {
+        required: false,
+      },
       control: {
         type: 'array',
       },
       table: {
         category: 'Visual',
-        type: 'text',
+        type: {
+          summary: ['string', 'Array<string>'].join(' | '),
+        },
       },
     },
     type: {
       description: 'The tag that is used to render out the component',
+      type: {
+        required: false,
+      },
       defaultValue: defaultHeadingType,
       control: {
         type: 'select',
@@ -79,17 +120,69 @@ export default {
       },
       table: {
         category: 'Structural',
-        type: 'text',
+        type: {
+          summary: headingTypes.join('|'),
+        },
         defaultValue: {
           summary: defaultHeadingType,
+        },
+      },
+    },
+    link: {
+      description: 'This prop can be used to render out the heading as a link',
+      type: {
+        required: false,
+      },
+      control: {
+        type: 'object',
+      },
+      table: {
+        category: 'Structural',
+        type: {
+          summary: 'object',
+          detail: '{ href: "string", target: "string" }',
         },
       },
     },
   },
 };
 
-export const Default = () => ({
+export const Default: Story<CFa1HeadingProps> = () => ({
   template: cfA1Heading,
 });
 
-Default.args = {};
+export const Link = Default.bind({});
+
+Link.args = {
+  link: {
+    href: '#',
+    target: '_blank',
+  },
+};
+
+export const Eyebrow = Default.bind({});
+
+Eyebrow.args = {
+  eyebrow: 'The quick brown fox jumps over the lazy dog.',
+};
+
+export const Mustache = Default.bind({});
+
+Mustache.args = {
+  mustache: 'The quick brown fox jumps over the lazy dog.',
+};
+
+export const EyebrowAndMustache = Default.bind({});
+
+EyebrowAndMustache.args = {
+  eyebrow: 'The quick brown fox jumps over the lazy dog.',
+  mustache: 'The quick brown fox jumps over the lazy dog.',
+};
+
+export const CustomTitles = Default.bind({});
+
+CustomTitles.args = {
+  eyebrow: 'The <u>quick</u> brown <strong>fox jumps</strong> over <i>the lazy</i> dog.',
+  title: 'The <u>quick</u> brown <strong>fox jumps</strong> over <i>the lazy</i> dog.',
+  mustache: 'The <u>quick</u> brown <strong>fox jumps</strong> over <i>the lazy</i> dog.',
+};
