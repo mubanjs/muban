a599beae8aae7454c20bc6e045d6d94bc6d76b72
Lars van Braam <lars@mediamonks.com>
2021-01-04 11:35:12 +0100

    Update the event listener hooks to actually remove the listeners

--- examples/src/hooks/useKeyboardEvent.ts
+++ examples/src/hooks/useKeyboardEvent.ts
@@ -1,20 +1,21 @@
 import { Key } from 'ts-key-enum';
+import { onMounted, onUnmounted } from '../../../src';
 
 type KeyPressCallback = (event: KeyboardEvent) => void;
 
-export const useKeyboardEvent = (
-  keys: Key | Array<Key>,
-  callback: KeyPressCallback,
-): (() => void) => {
+export const useKeyboardEvent = (keys: Key | Array<Key>, callback: KeyPressCallback): void => {
   const onKeydown = (event: KeyboardEvent) => {
     if (Array.isArray(keys) ? keys.includes(event.key as Key) : event.key === keys) {
       callback(event);
     }
   };
 
-  document.addEventListener('keydown', onKeydown);
-
-  return () => document.removeEventListener('keydown', onKeydown);
+  onMounted(() => {
+    document.addEventListener('keydown', onKeydown);
+  });
+  onUnmounted(() => {
+    document.removeEventListener('keydown', onKeydown);
+  });
 };
 
 export const useEscapeKeyEvent = (callback: KeyPressCallback) =>
