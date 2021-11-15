d7ab80d5e67b673562b7ad6699648b485cef3cc2
Arjan van Wijk <thanarie@gmail.com>
2021-11-15 13:18:59 +0100

    Exclude .types.test.ts file patterns in Jest

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   // testEnvironment: 'jsdom',
   transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)'],
   setupFilesAfterEnv: ['<rootDir>/test-utils/setupTests.js'],
+  // added "(?<!types.)" as a negative lookbehind to the default pattern
+  // to exclude .types.test.ts patterns fro being picked up by jest
+  testRegex: '(/__tests__/.*|(\\.|/)(?<!types.)(test|spec))\\.[jt]sx?$'
 };
