91efc5af2474f1e5b4959ad097ca942453e887da
Thijs <thijs@mediamonks.com>
2021-11-26 15:55:54 +0100

    Fix some typing issues that were causing build error

--- src/lib/bindings/dom/submitBinding.ts
+++ src/lib/bindings/dom/submitBinding.ts
@@ -2,7 +2,7 @@ export function submitBinding(
   target: HTMLFormElement,
   fn: (event: HTMLElementEventMap['submit']) => void | boolean,
 ) {
-  const submitHandler = (event: Event) => {
+  const submitHandler = (event: SubmitEvent) => {
     const returnValue = fn(event);
     // by default, prevent default form execution, unless explicitly returned by the handler
     if (returnValue !== true) {
