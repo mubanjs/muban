7a93997016092708d3fdacca039fdbf7b0316a6d
Arjan van Wijk <thanarie@gmail.com>
2021-12-15 12:56:37 +0100

    Proxy watch/watchEffect to allow auto-cleanup on component unmount
    `watch` and `watchEffect` exist in runtime-core instead of reactivity
    packages because they are tied to the Vue components, and their
    lifecycles. Which has some advantages, that Muban didn't have.
    
    With this update, we proxy those two functions to allow for auto-cleanup
    when the Muban component unmounts. It will also ignore usages when
    invoked in an unmounted component.
    
    For library consumers this should work out of the box, as long as they
    use the muban exports (and not the vue exports directly).

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,17 @@ export { propType } from './lib/props/propDefinitions';
 export { refElement, refCollection, refComponent, refComponents } from './lib/refs/refDefinitions';
 export { provide, inject, createContext } from './lib/api/apiInject';
 export { onMounted, onUnmounted } from './lib/api/apiLifecycle';
+export { watch, watchEffect } from './lib/api/apiWatch';
 export { bind, bindMap, bindTemplate } from './lib/bindings/bindingDefinitions';
 
 // re-export types of those libs, so they don't have to be installed separately
 export * from '@vue/reactivity';
-export {
-  watch,
-  watchEffect, // WatchCallback, // WatchEffect, // WatchOptions, // WatchSource, // WatchStopHandle,
-} from '@vue/runtime-core';
+
+// TODO: should we export types from runtime/core?
+// export {
+//   watch,
+//   watchEffect, // WatchCallback, // WatchEffect, // WatchOptions, // WatchSource, // WatchStopHandle,
+// } from '@vue/runtime-core';
 
 // TODO: devtools
 initDev();
