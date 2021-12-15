6ee4c4f75a1afb7c422996e7fa0fa3d47b6b53ae
Arjan van Wijk <thanarie@gmail.com>
2021-12-15 12:56:37 +0100

    Proxy watch/watchEffect to allow auto-cleanup on component unmount
    `watch` and `watchEffect` exist in runtime-core instead of reactivity
    packages because they are tied to the Vue components, and their
    lifecycles. Which has some advantages, that Muban didn't have.
    
    With this update, we proxy those two functions to allow for auto-cleanup
    when the Muban component unmounts. It will also ignore usages when
    invoked in an unmounted component.
    
    For library consumers this should work out of the box, as long as they
    use the muban exports (and not the vue exports directly).

---
+++ src/lib/api/apiWatch.ts
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import {
+  watch as vueWatch,
+  watchEffect as vueWatchEffect,
+  WatchCallback,
+  WatchEffect,
+  WatchOptions,
+  WatchOptionsBase,
+  WatchSource,
+  WatchStopHandle,
+} from '@vue/runtime-core';
+import { getCurrentComponentInstance } from '../Component';
+
+declare type MapSources<T, Immediate> = {
+  [K in keyof T]: T[K] extends WatchSource<infer V>
+    ? Immediate extends true
+      ? V | undefined
+      : V
+    : T[K] extends object
+    ? Immediate extends true
+      ? T[K] | undefined
+      : T[K]
+    : never;
+};
+declare type MultiWatchSources = Array<WatchSource<unknown> | object>;
+
+export function watch<T extends MultiWatchSources, Immediate extends Readonly<boolean> = false>(
+  sources: [...T],
+  cb: WatchCallback<MapSources<T, false>, MapSources<T, Immediate>>,
+  options?: WatchOptions<Immediate>,
+): WatchStopHandle;
+export function watch<
+  T extends Readonly<MultiWatchSources>,
+  Immediate extends Readonly<boolean> = false
+>(
+  source: T,
+  cb: WatchCallback<MapSources<T, false>, MapSources<T, Immediate>>,
+  options?: WatchOptions<Immediate>,
+): WatchStopHandle;
+export function watch<T, Immediate extends Readonly<boolean> = false>(
+  source: WatchSource<T>,
+  cb: WatchCallback<T, Immediate extends true ? T | undefined : T>,
+  options?: WatchOptions<Immediate>,
+): WatchStopHandle;
+export function watch<T extends object, Immediate extends Readonly<boolean> = false>(
+  source: T,
+  cb: WatchCallback<T, Immediate extends true ? T | undefined : T>,
+  options?: WatchOptions<Immediate>,
+): WatchStopHandle;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function watch(sources: any, cb: any, options: any): WatchStopHandle {
+  const instance = getCurrentComponentInstance();
+  // don't even create the effect if this component is not mounted
+  if (instance && instance.isUnmounted) {
+    return () => undefined;
+  }
+
+  const stopHandle = vueWatch(sources, cb, options);
+
+  // register stopHandle so it gets executed when the component unmounts
+  instance?.disposers.push(stopHandle);
+
+  return stopHandle;
+}
+
+export function watchEffect(effect: WatchEffect, options?: WatchOptionsBase): WatchStopHandle {
+  const instance = getCurrentComponentInstance();
+  // don't even create the effect if this component is not mounted
+  if (instance && instance.isUnmounted) {
+    return () => undefined;
+  }
+
+  const stopHandle = vueWatchEffect(effect, options);
+
+  // register stopHandle so it gets executed when the component unmounts
+  instance?.disposers.push(stopHandle);
+
+  return stopHandle;
+}
