57fcfeadf62f93cf2caa657e7554f7fb35b3c3cd
Arjan van Wijk <thanarie@gmail.com>
2022-01-25 19:45:54 +0100

    Make lazy type-safe and add runtime checking
    The imported file from `getComponent` now must have an export name that
    is either the `displayName` in `PascalCase`, or the `exportName`. This
    is enforced in TS, and shows a mismatch error if that's not the case.
    
    If you wanted to, you could still choose to ignore the types somehow,
    resulting in a runtime error. That scenario is now properly checked, and
    shows a clear error message.

--- src/lib/api/apiLazy.ts
+++ src/lib/api/apiLazy.ts
@@ -1,12 +1,28 @@
 import { pascalCase } from 'change-case';
+import type { PascalCase } from 'type-fest';
 import type { ComponentFactory, LazyComponent } from '../Component.types';
 
-export function lazy(
-  displayName: string,
-  getComponent: () => Promise<{ [key: string]: ComponentFactory }>,
-  exportName?: string,
+type ComponentExportName<T extends string, U extends string | undefined> = undefined extends U
+  ? PascalCase<T>
+  : Exclude<U, undefined>;
+
+export function lazy<T extends string, U extends string | undefined>(
+  displayName: T,
+  getComponent: () => Promise<{ [Key in ComponentExportName<T, U>]: ComponentFactory }>,
+  exportName: U = pascalCase(displayName) as U,
 ): LazyComponent {
-  const fn = async () => (await getComponent())[exportName || pascalCase(displayName)];
+  const fn = async () => {
+    const exports = await getComponent();
+    const factory = exports[exportName as ComponentExportName<T, U>];
+    if (!factory) {
+      throw new Error(
+        `Error after lazy loading.\nLazy loaded component "${displayName}" doesn't have export "${exportName}".\nDid you mean one of [${Object.keys(
+          exports,
+        )}]`,
+      );
+    }
+    return factory;
+  };
 
   fn.displayName = displayName;
   fn.isLazy = true as const;
