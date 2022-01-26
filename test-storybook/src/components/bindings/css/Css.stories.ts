a0a66c9ede7e0b281a371f4d9b078b81d94d9322
Pablo Gimenez <pablo.gimenez@mediamonks.com>
2022-01-26 17:00:12 -0300

    Improve CSS binding story tests
    Refactor story component and story for a cleaner code.
    Move the html template to a function and use 'refCollection' instead of 'ref' to bind the checkboxes to reduce code.
    Add a locked class to the box element for testing purposes.
    Replace previous 'Story' with 'Story' in the CFSv3 (object) format.
    Add interaction testing using the 'play' function for a more useful test.
    Remove eslint disable rule comment.

--- test-storybook/src/components/bindings/css/Css.stories.ts
+++ test-storybook/src/components/bindings/css/Css.stories.ts
@@ -1,78 +1,116 @@
-/* eslint-disable max-lines */
 import { html } from '@muban/template';
-import type { Story } from '@muban/storybook/types-6-0';
-import { bind, defineComponent, computed, ref } from '@muban/muban';
+import type { Story } from '@muban/storybook';
+import {
+  bind,
+  defineComponent,
+  computed,
+  ref,
+  refCollection,
+  ComponentFactory,
+} from '@muban/muban';
+import { screen, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import type { Binding } from '../../../../../types/lib/bindings/bindings.types';
 
 export default {
   title: 'bindings/css',
 };
 
-export const CssObject: Story = () => ({
-  component: defineComponent({
-    name: 'object',
-    refs: {
-      box: 'box',
-      info: 'info',
-      checkbox1: 'checkbox1',
-      checkbox2: 'checkbox2',
-      checkbox3: 'checkbox3',
-    },
-    setup({ refs }) {
-      const checked1 = ref<boolean>(true);
-      const checked2 = ref<boolean>(false);
-      const checked3 = ref<boolean>(false);
-      return [
-        bind(refs.info, { text: computed(() => `${checked1.value ? "'bg-primary': ref(true), " : ''} ${checked2.value ? "'text-success': ref(true), " : ''} ${checked3.value ? "'fs-3': ref(true), " : ''}` ) } ),
-        bind(refs.box, { css: { 'bg-primary': checked1, 'text-success': checked2, 'fs-3': checked3 } }),
-        bind(refs.checkbox1, { checked: checked1 }),
-        bind(refs.checkbox2, { checked: checked2 }),
-        bind(refs.checkbox3, { checked: checked3 }),
-      ];
-    },
-  }),
-  template: () => html` <div data-component="object">
-  <p>Css Object</p>
-    <div data-ref="box" class="bg-primary">
-      <p>css: { <span data-ref="info"></span> }</p>
-    </div>
-    <p><input data-ref="checkbox1" type="checkbox" value="foo" checked /> .bg-primary</p>
-    <p><input data-ref="checkbox2" type="checkbox" value="foo" /> .text-success</p>
-    <p><input data-ref="checkbox3" type="checkbox" value="foo" /> .fs-3</p>
-  </div>`,
-});
-
-export const CssString: Story = () => ({
-  component: defineComponent({
-    name: 'string',
-    refs: {
-      box: 'box',
-      info: 'info',
-      checkbox1: 'checkbox1',
-      checkbox2: 'checkbox2',
-      checkbox3: 'checkbox3',
-    },
-    setup({ refs }) {
-      const checked1 = ref<boolean>(false);
-      const checked2 = ref<boolean>(true);
-      const checked3 = ref<boolean>(false);
-      return [
-        bind(refs.info, { text: computed(() => `'${checked1.value ? 'bg-primary' : ''} ${checked2.value ? 'text-success' : ''} ${checked3.value ? 'fs-3' : ''}'` ) } ),
-        bind(refs.box, { css: computed(() =>
-          `${checked1.value && 'bg-primary' } ${checked2.value && 'text-success'} ${checked3.value && 'fs-3'}`
-        )}),
-        bind(refs.checkbox1, { checked: checked1 }),
-        bind(refs.checkbox2, { checked: checked2 }),
-        bind(refs.checkbox3, { checked: checked3 }),
-      ];
-    },
-  }),
-  template: () => html` <div data-component="string">
-  <p>Css String</p>
-    <div data-ref="box" class="text-success">
-    <p>css: computed (() => <span data-ref="info"></span>)</p>
+const cssTemplate = () => html` <div data-component="css">
+  <div data-ref="box" data-testid="box" class="box bg-primary">
+    <p>css: <span data-ref="info"></span></p>
   </div>
-  <p><input data-ref="checkbox1" type="checkbox" value="foo" /> .bg-primary</p>
-  <p><input data-ref="checkbox2" type="checkbox" value="foo" checked /> .text-success</p>
-  <p><input data-ref="checkbox3" type="checkbox" value="foo" /> .fs-3</p>
-  </div>`,
-});
+  <p>
+    <input data-ref="checkbox" data-testid="checkbox-box" type="checkbox" value="box" disabled />
+    .box
+  </p>
+  <p>
+    <input
+      data-ref="checkbox"
+      data-testid="checkbox-bg-primary"
+      type="checkbox"
+      value="bg-primary"
+    />
+    .bg-primary
+  </p>
+  <p>
+    <input
+      data-ref="checkbox"
+      data-testid="checkbox-text-success"
+      type="checkbox"
+      value="text-success"
+    />
+    .text-success
+  </p>
+  <p>
+    <input data-ref="checkbox" data-testid="checkbox-fs-3" type="checkbox" value="fs-3" /> .fs-3
+  </p>
+</div>`;
+
+const interactiveTest = async () => {
+  await expect(screen.getByTestId('box').classList.value).toBe('box bg-primary');
+  await userEvent.click(screen.getByTestId('checkbox-text-success'));
+  await expect(screen.getByTestId('box').classList.value).toBe('box bg-primary text-success');
+  await userEvent.click(screen.getByTestId('checkbox-fs-3'));
+  await expect(screen.getByTestId('box').classList.value).toBe('box bg-primary text-success fs-3');
+  await userEvent.click(screen.getByTestId('checkbox-bg-primary'));
+  await expect(screen.getByTestId('box').classList.value).toBe('box text-success fs-3');
+  await userEvent.click(screen.getByTestId('checkbox-text-success'));
+  await expect(screen.getByTestId('box').classList.value).toBe('box fs-3');
+  await userEvent.click(screen.getByTestId('checkbox-fs-3'));
+  await expect(screen.getByTestId('box').classList.value).toBe('box');
+};
+
+const createCssComponent = (setup: (context) => Array<Binding>) => {
+  return defineComponent({
+    name: 'css',
+    refs: {
+      box: 'box',
+      info: 'info',
+      checkboxes: refCollection('checkbox'),
+    },
+    setup,
+  });
+};
+
+const createCssStory = (component: ComponentFactory<any>) => {
+  return {
+    render() {
+      return {
+        component,
+        template: cssTemplate,
+      };
+    },
+    play: interactiveTest,
+  };
+};
+
+export const CssObject: Story = createCssStory(
+  createCssComponent(({ refs }) => {
+    const classList = Array.from(
+      refs.checkboxes.getElements(),
+      (checkbox) => (<HTMLInputElement>checkbox).defaultValue,
+    );
+    const checkedClasses = ref(['box', 'bg-primary']);
+    const selectedClasses = computed(() =>
+      classList.reduce((o, key) => ({ ...o, [key]: checkedClasses.value.includes(key) }), {}),
+    );
+    return [
+      bind(refs.info, { text: computed(() => JSON.stringify(selectedClasses.value)) }),
+      bind(refs.box, { css: selectedClasses }),
+      bind(refs.checkboxes, { checked: checkedClasses, initialValueSource: 'binding' }),
+    ];
+  }),
+);
+
+export const CssString: Story = createCssStory(
+  createCssComponent(({ refs }) => {
+    const checkedClasses = ref(['box', 'bg-primary']);
+    const classList = computed(() => checkedClasses.value.join(' '));
+    return [
+      bind(refs.info, { text: classList }),
+      bind(refs.box, { css: classList }),
+      bind(refs.checkboxes, { checked: checkedClasses, initialValueSource: 'binding' }),
+    ];
+  }),
+);
