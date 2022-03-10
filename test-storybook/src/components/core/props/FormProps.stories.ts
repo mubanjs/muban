76c976c352b73a43ba79d80b848cf44cf893dc94
Juan Polanco <jspolancor@gmail.com>
2022-03-10 13:22:57 -0500

    Update stories for createFormPropertySource()

--- test-storybook/src/components/core/props/FormProps.stories.ts
+++ test-storybook/src/components/core/props/FormProps.stories.ts
@@ -9,12 +9,10 @@ export default {
   title: 'core/props/form',
 };
 
-const getInfoBinding = (refs: any, props: any) => {
-  console.log(props);
-  return bind(refs.info, {
+const getInfoBinding = (refs: any, props: any) =>
+  bind(refs.info, {
     text: computed(() => JSON.stringify(props, null, 2)),
   });
-};
 
 const createPropsComponent = (
   props: Record<string, PropTypeDefinition>,
@@ -33,13 +31,6 @@ const createPropsComponent = (
   });
 };
 
-/* string
-number
-boolean
-date
-object
-array */
-
 export const Form: Story = () => ({
   component: createPropsComponent(
     {
@@ -49,6 +40,24 @@ export const Form: Story = () => ({
       inputDate: propType.date.source({ type: 'form', target: 'inputDateRef' }),
       inputObject: propType.object.source({ type: 'form', target: 'inputObjectRef' }),
       inputArray: propType.array.source({ type: 'form', target: 'inputArrayRef' }),
+      checkboxOnBoolean: propType.boolean.source({ type: 'form', target: 'checkboxOnBooleanRef' }),
+      checkboxOnString: propType.string.source({ type: 'form', target: 'checkboxOnStringRef' }),
+      checkboxOnValueString: propType.string.source({
+        type: 'form',
+        target: 'checkboxOnValueStringRef',
+      }),
+      checkboxOffBoolean: propType.boolean.source({
+        type: 'form',
+        target: 'checkboxOffBooleanRef',
+      }),
+      checkboxOffString: propType.string.source({ type: 'form', target: 'checkboxOffStringRef' }),
+      checkboxOffValueString: propType.string.source({
+        type: 'form',
+        target: 'checkboxOffValueStringRef',
+      }),
+      selectText: propType.string.source({ type: 'form', target: 'selectRef' }),
+      multiSelectText: propType.number.source({ type: 'form', target: 'multiSelectRef' }),
+      formData: propType.object.source({ type: 'form', target: 'formRef' }),
     },
     {
       inputTextRef: 'inputTextRef',
@@ -57,16 +66,45 @@ export const Form: Story = () => ({
       inputDateRef: 'inputDateRef',
       inputObjectRef: 'inputObjectRef',
       inputArrayRef: 'inputArrayRef',
+      checkboxOnBooleanRef: 'checkboxOnBooleanRef',
+      checkboxOnStringRef: 'checkboxOnStringRef',
+      checkboxOnValueStringRef: 'checkboxOnValueStringRef',
+      checkboxOffBooleanRef: 'checkboxOffBooleanRef',
+      checkboxOffStringRef: 'checkboxOffStringRef', // This wont show up in the info as it's undefined
+      checkboxOffValueStringRef: 'checkboxOffValueStringRef', // This wont show up in the info as it's undefined
+      selectRef: 'selectRef',
+      multiSelectRef: 'multiSelectRef',
+      formRef: 'formRef',
     },
   ),
-  template: () => html`<form data-component="props">
-    <h2>Input</h2>
-    <input data-ref="inputTextRef" type="email" value="juan.polanco@mediamonks.com" /><br />
-    <input data-ref="inputNumberRef" type="number" value="31" /><br />
-    <input data-ref="inputBooleanRef" type="text" value="true" /><br />
-    <input data-ref="inputDateRef" type="text" value="2022-01-01" /><br />
-    <input data-ref="inputObjectRef" type="text" value='{"foo": "bar"}' /><br />
-    <input data-ref="inputArrayRef" type="text" value="[1, 2, 3, 4]" /><br />
-    <pre data-ref="info"></pre>
-  </form>`,
+  template: () => html`<div data-component="props">
+    <form data-ref="formRef">
+      <input
+        data-ref="inputTextRef"
+        name="email"
+        type="email"
+        value="juan.polanco@mediamonks.com"
+      /><br />
+      <input data-ref="inputNumberRef" name="age" type="number" value="31" /><br />
+      <input data-ref="inputBooleanRef" type="text" value="true" /><br />
+      <input data-ref="inputDateRef" type="text" value="2022-01-01" /><br />
+      <input data-ref="inputObjectRef" type="text" value='{"foo": "bar"}' /><br />
+      <input data-ref="inputArrayRef" type="text" value="[1, 2, 3, 4]" /><br />
+      <input data-ref="checkboxOnBooleanRef" type="checkbox" checked /><br />
+      <input data-ref="checkboxOnStringRef" type="checkbox" checked /><br />
+      <input data-ref="checkboxOnValueStringRef" type="checkbox" value="foo" checked /><br />
+      <input data-ref="checkboxOffBooleanRef" type="checkbox" /><br />
+      <input data-ref="checkboxOffStringRef" type="checkbox" /><br />
+      <input data-ref="checkboxOffValueStringRef" type="checkbox" value="foo" /><br />
+      <select data-ref="selectRef">
+        <option value="foo" selected>foo</option>
+        <option value="bar">bar</option>
+      </select>
+      <select data-ref="multiSelectRef" multiple>
+        <option value="foo" selected>foo</option>
+        <option value="bar">bar</option>
+      </select>
+      <pre data-ref="info"></pre>
+    </form>
+  </div>`,
 });
