1d2179f13c7ed2f48c15cfad2fce77ef3d4e47d0
Juan Polanco <jspolancor@gmail.com>
2022-03-23 09:20:02 -0500

    Update createFormPropertySource tests to have an isolated testing form per unit test

--- src/lib/props/property-sources/createFormPropertySource.test.ts
+++ src/lib/props/property-sources/createFormPropertySource.test.ts
@@ -1,30 +1,6 @@
 import type { PropTypeInfo } from '../propDefinitions.types';
 import { createFormPropertySource } from './createFormPropertySource';
 
-const form = document.createElement('form');
-form.innerHTML = `
-  <input id="email" type="email" name="email" value="juan.polanco@mediamonks.com"/>
-  <input id="password" type="password" name="password" value="123456"/>
-  <textarea id="description" name="description">lorem ipsum</textarea>
-  <select id="preference" name="preference">
-    <option value="foo" selected>foo</option>
-    <option value="bar">bar</option>
-  </select>
-  <select id="preferenceBoolean" name="preferenceBoolean">
-    <option value="true" selected>foo</option>
-    <option value="false">bar</option>
-  </select>
-  <input id="optionA" name="optionA" type="checkbox" />
-  <input id="optionB" name="optionB" type="checkbox" value="foo" checked />
-  <input id="choices" name="choices" type="checkbox" value="apple" checked/>
-  <input id="checkedChoices" name="choices" type="checkbox" value="banana" checked />
-  <select id="candidates" name="candidates" multiple>
-   <option value="foo" selected>foo</option>
-    <option value="bar" selected>bar</option>
-  </select>
-  <input id="photo" name="photo" type="file" />
-`;
-
 describe('createFormPropertySource', () => {
   describe('function itself', () => {
     it('should create without errors', () => {
@@ -36,6 +12,7 @@ describe('createFormPropertySource', () => {
   });
 
   describe('hasProp', () => {
+    const form = document.createElement('form');
     it('should return false if the type is "Function"', () => {
       const functionPropInfo: PropTypeInfo = {
         name: 'email',
@@ -49,6 +26,7 @@ describe('createFormPropertySource', () => {
       expect(createFormPropertySource()(form).hasProp(functionPropInfo)).toBe(false);
     });
     it('should return true if the type is different than "Function"', () => {
+      const form = document.createElement('form');
       const validPropInfoTypes = [Number, String, Boolean, Date, Array, Object];
       const propInfos: Array<PropTypeInfo> = validPropInfoTypes.map((type) => ({
         name: 'foo',
@@ -66,7 +44,13 @@ describe('createFormPropertySource', () => {
   });
 
   describe('getProp', () => {
-    it('Should return the input value if the target is an input and the type is not "checkbox"', () => {
+    it('Should return the input value if the target is a text input', () => {
+      const form = document.createElement('form');
+      form.innerHTML = `
+        <input id="email" type="email" name="email" value="juan.polanco@mediamonks.com"/>
+        <input id="password" type="password" name="password" value="123456"/>
+        <textarea id="description" name="description">lorem ipsum</textarea>
+      `;
       const emailInput: PropTypeInfo = {
         name: 'email',
         type: String,
@@ -97,7 +81,13 @@ describe('createFormPropertySource', () => {
       expect(createFormPropertySource()(form).getProp(passwordInput)).toBe('123456');
       expect(createFormPropertySource()(form).getProp(descriptionInput)).toBe('lorem ipsum');
     });
+
     it('Should return the checked value if the target is a checkbox and the propType is boolean', () => {
+      const form = document.createElement('form');
+      form.innerHTML = `
+        <input id="optionA" name="optionA" type="checkbox" />
+        <input id="optionB" name="optionB" type="checkbox" value="foo" checked />
+      `;
       const optionA: PropTypeInfo = {
         name: 'checkbox',
         type: Boolean,
@@ -117,7 +107,12 @@ describe('createFormPropertySource', () => {
       expect(createFormPropertySource()(form).getProp(optionA)).toBe(false);
       expect(createFormPropertySource()(form).getProp(optionB)).toBe(true);
     });
+
     it('Should return the input value if the target is a checked checkbox and the propType is not boolean', () => {
+      const form = document.createElement('form');
+      form.innerHTML = `
+        <input id="optionB" name="optionB" type="checkbox" value="foo" checked />
+      `;
       const optionB: PropTypeInfo = {
         name: 'checkbox',
         type: String,
@@ -128,7 +123,13 @@ describe('createFormPropertySource', () => {
       };
       expect(createFormPropertySource()(form).getProp(optionB)).toBe('foo');
     });
+
     it('Should return an array of strings if the target is checkbox and the propType is Array', () => {
+      const form = document.createElement('form');
+      form.innerHTML = `
+        <input name="choices" type="checkbox" value="apple" checked/>
+        <input name="choices" type="checkbox" value="banana" checked />
+      `;
       const choices: PropTypeInfo = {
         name: 'checkbox',
         type: Array,
@@ -142,7 +143,19 @@ describe('createFormPropertySource', () => {
         JSON.stringify(['apple', 'banana']),
       );
     });
+
     it('Should return the select value if the target is a select that is not multiple', () => {
+      const form = document.createElement('form');
+      form.innerHTML = `
+        <select id="preference" name="preference">
+          <option value="foo" selected>foo</option>
+          <option value="bar">bar</option>
+        </select>
+        <select id="preferenceBoolean" name="preferenceBoolean">
+          <option value="true" selected>foo</option>
+          <option value="false">bar</option>
+        </select>
+      `;
       const directSelect: PropTypeInfo = {
         name: 'select',
         type: String,
@@ -173,7 +186,15 @@ describe('createFormPropertySource', () => {
       expect(createFormPropertySource()(form).getProp(directSelect)).toBe('foo');
       expect(createFormPropertySource()(form).getProp(formDataSelect)).toBe('foo');
     });
+
     it('Should return an array of strings if the target is a multiselect', () => {
+      const form = document.createElement('form');
+      form.innerHTML = `
+        <select id="candidates" name="candidates" multiple>
+          <option value="foo" selected>foo</option>
+          <option value="bar" selected>bar</option>
+        </select>
+      `;
       const candidates: PropTypeInfo = {
         name: 'multiselect',
         type: Array,
@@ -198,29 +219,9 @@ describe('createFormPropertySource', () => {
         JSON.stringify(createFormPropertySource()(form).getProp(candidatesFromFormData)),
       ).toStrictEqual(JSON.stringify(['foo', 'bar']));
     });
-    it('Should return a File if the target is an input with type "file"', () => {
-      const photo: PropTypeInfo = {
-        name: 'file',
-        type: Object,
-        source: {
-          target: form.querySelector<HTMLElement>('#photo')!,
-          type: 'form',
-        },
-      };
-      expect(createFormPropertySource()(form).getProp(photo)).toBe(undefined);
-    });
-    it('Should return a FileList if the target is an input with type "file" and is type Array', () => {
-      const photoArray: PropTypeInfo = {
-        name: 'file',
-        type: Array,
-        source: {
-          target: form.querySelector<HTMLElement>('#photo')!,
-          type: 'form',
-        },
-      };
-      expect(createFormPropertySource()(form).getProp(photoArray)).toBe(undefined);
-    });
+
     it('Should return undefined when trying to get FormData not using type "Object"', () => {
+      const form = document.createElement('form');
       const wrongTypePropInfo: PropTypeInfo = {
         name: 'myForm',
         type: String, // This should be Object
@@ -231,7 +232,12 @@ describe('createFormPropertySource', () => {
       };
       expect(createFormPropertySource()(form).getProp(wrongTypePropInfo)).toBe(undefined);
     });
+
     it('Should return FormData when using type "Object" and an unnamed source', () => {
+      const form = document.createElement('form');
+      form.innerHTML = `
+        <input id="email" type="email" name="email" value="juan.polanco@mediamonks.com"/>
+      `;
       const validForm: PropTypeInfo = {
         name: 'myForm',
         type: Object,
@@ -245,7 +251,12 @@ describe('createFormPropertySource', () => {
         'juan.polanco@mediamonks.com',
       );
     });
+
     it('Should return the input value when passing a form element and a named source', () => {
+      const form = document.createElement('form');
+      form.innerHTML = `
+        <input id="email" type="email" name="email" value="juan.polanco@mediamonks.com"/>
+      `;
       const validForm: PropTypeInfo = {
         name: 'myForm',
         type: String,
