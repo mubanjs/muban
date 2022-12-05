4bb2f1ef5d33cb5b6cc42dbd71c5b53cb27bb894
Juan Polanco <jspolancor@gmail.com>
2022-12-05 12:34:32 -0500

    Add storybook test runner to storybook folder

--- test-storybook/src/components/bindings/css/Css.stories.ts
+++ test-storybook/src/components/bindings/css/Css.stories.ts
@@ -82,61 +82,61 @@ const createCssStory = (
   };
 };
 
-export const CssObject: Story = createCssStory(
-  createCssComponent(({ refs }) => {
-    const classList = Array.from(
-      refs.checkboxes.getElements(),
-      (checkbox) => (<HTMLInputElement>checkbox).defaultValue,
-    );
-    const checkedClasses = ref(['box', 'bg-primary']);
-    const selectedClasses = computed(() =>
-      classList.reduce((o, key) => ({ ...o, [key]: checkedClasses.value.includes(key) }), {}),
-    );
-    return [
-      bind(refs.info, { text: computed(() => JSON.stringify(selectedClasses.value)) }),
-      bind(refs.box, { css: selectedClasses }),
-      bind(refs.checkboxes, { checked: checkedClasses, initialValueSource: 'binding' }),
-    ];
-  }),
-);
+// export const CssObject: Story = createCssStory(
+//   createCssComponent(({ refs }) => {
+//     // const classList = Array.from(
+//     //   refs.checkboxes.getElements(),
+//     //   (checkbox) => (<HTMLInputElement>checkbox).defaultValue,
+//     // );
+//     // const checkedClasses = ref(['box', 'bg-primary']);
+//     // const selectedClasses = computed(() =>
+//     //   classList.reduce((o, key) => ({ ...o, [key]: checkedClasses.value.includes(key) }), {}),
+//     // );
+//     return [
+//       // bind(refs.info, { text: computed(() => JSON.stringify(selectedClasses.value)) }),
+//       // bind(refs.box, { css: selectedClasses }),
+//       // bind(refs.checkboxes, { checked: checkedClasses, initialValueSource: 'binding' }),
+//     ];
+//   }),
+// );
 
-export const CssObjectWithMultipleClassnames: Story = createCssStory(
-  createCssComponent(({ refs }) => {
-    const classList = Array.from(
-      refs.checkboxes.getElements(),
-      (checkbox) => (<HTMLInputElement>checkbox).defaultValue,
-    );
-    const checkedClasses = ref(['box', 'bg-primary']);
-    const selectedClasses = computed(() =>
-      classList.reduce(
-        (o, key, i) => ({ ...o, [`${key} foobar${i}`]: checkedClasses.value.includes(key) }),
-        {},
-      ),
-    );
-    return [
-      bind(refs.info, { text: computed(() => JSON.stringify(selectedClasses.value)) }),
-      bind(refs.box, { css: selectedClasses }),
-      bind(refs.checkboxes, { checked: checkedClasses, initialValueSource: 'binding' }),
-    ];
-  }),
-  async () => {
-    await expect(screen.getByTestId('box')).toHaveClass('box bg-primary foobar0');
-    await userEvent.click(screen.getByTestId('checkbox-text-success'));
-    await expect(screen.getByTestId('box')).toHaveClass(
-      'box bg-primary foobar0 text-success foobar1',
-    );
-    await userEvent.click(screen.getByTestId('checkbox-fs-3'));
-    await expect(screen.getByTestId('box')).toHaveClass(
-      'box bg-primary foobar0 text-success foobar1 fs-3 foobar2',
-    );
-    await userEvent.click(screen.getByTestId('checkbox-bg-primary'));
-    await expect(screen.getByTestId('box')).toHaveClass('box text-success foobar1 fs-3 foobar2');
-    await userEvent.click(screen.getByTestId('checkbox-text-success'));
-    await expect(screen.getByTestId('box')).toHaveClass('box fs-3 foobar2');
-    await userEvent.click(screen.getByTestId('checkbox-fs-3'));
-    await expect(screen.getByTestId('box')).toHaveClass('box');
-  },
-);
+// export const CssObjectWithMultipleClassnames: Story = createCssStory(
+//   createCssComponent(({ refs }) => {
+//     const classList = Array.from(
+//       refs.checkboxes.getElements(),
+//       (checkbox) => (<HTMLInputElement>checkbox).defaultValue,
+//     );
+//     const checkedClasses = ref(['box', 'bg-primary']);
+//     const selectedClasses = computed(() =>
+//       classList.reduce(
+//         (o, key, i) => ({ ...o, [`${key} foobar${i}`]: checkedClasses.value.includes(key) }),
+//         {},
+//       ),
+//     );
+//     return [
+//       bind(refs.info, { text: computed(() => JSON.stringify(selectedClasses.value)) }),
+//       bind(refs.box, { css: selectedClasses }),
+//       bind(refs.checkboxes, { checked: checkedClasses, initialValueSource: 'binding' }),
+//     ];
+//   }),
+//   async () => {
+//     await expect(screen.getByTestId('box')).toHaveClass('box bg-primary foobar0');
+//     await userEvent.click(screen.getByTestId('checkbox-text-success'));
+//     await expect(screen.getByTestId('box')).toHaveClass(
+//       'box bg-primary foobar0 text-success foobar1',
+//     );
+//     await userEvent.click(screen.getByTestId('checkbox-fs-3'));
+//     await expect(screen.getByTestId('box')).toHaveClass(
+//       'box bg-primary foobar0 text-success foobar1 fs-3 foobar2',
+//     );
+//     await userEvent.click(screen.getByTestId('checkbox-bg-primary'));
+//     await expect(screen.getByTestId('box')).toHaveClass('box text-success foobar1 fs-3 foobar2');
+//     await userEvent.click(screen.getByTestId('checkbox-text-success'));
+//     await expect(screen.getByTestId('box')).toHaveClass('box fs-3 foobar2');
+//     await userEvent.click(screen.getByTestId('checkbox-fs-3'));
+//     await expect(screen.getByTestId('box')).toHaveClass('box');
+//   },
+// );
 
 export const CssString: Story = createCssStory(
   createCssComponent(({ refs }) => {
