d71f703782d1f202d18709de30691c857aba9ad0
Juan Polanco <jspolancor@gmail.com>
2022-12-21 09:07:36 -0500

    Add .play function to the HtmlProps stories

--- test-storybook/src/components/core/props/HtmlProps.stories.ts
+++ test-storybook/src/components/core/props/HtmlProps.stories.ts
@@ -3,6 +3,9 @@ import type { Story } from '@muban/storybook/types-6-0';
 import { html } from '@muban/template';
 import { bind, defineComponent, propType, computed } from '@muban/muban';
 import type { PropTypeDefinition, ComponentRefItem } from '@muban/muban';
+import { screen, queryByAttribute } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import isValidJson from '../../../utils/isValidJson';
 
 export default {
   title: 'core/props/html',
@@ -41,9 +44,20 @@ export const HtmlString: Story = () => ({
       props2: 'props2',
     },
   ),
-  template: () => html` <div data-component="props">
+  template: () => html` <div data-component="props" data-testid="props-html-string-story">
     <div data-ref="props">success</div>
     <div data-ref="props2">Hello <strong>world</strong>!</div>
     <pre data-ref="info"></pre>
   </div>`,
 });
+HtmlString.play = async () => {
+  const storyContainer = screen.getByTestId('props-html-string-story')!;
+  const props = queryByAttribute('data-ref', storyContainer, 'props') as HTMLDivElement;
+  const props2 = queryByAttribute('data-ref', storyContainer, 'props2') as HTMLDivElement;
+  const info = queryByAttribute('data-ref', storyContainer, 'info') as HTMLPreElement;
+  expect(info.textContent).not.toBe(undefined);
+  expect(isValidJson(info.textContent!)).toBe(true);
+  const parsedInfo = JSON.parse(info.textContent!);
+  expect(parsedInfo.statusValue).toBe(props.innerHTML);
+  expect(parsedInfo.statusValue2).toBe(props2.innerHTML);
+};
