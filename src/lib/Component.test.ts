c759a5c207ceb6c16e49166e5468f18b84e93e81
Juan Polanco <jspolancor@gmail.com>
2023-02-20 22:05:48 -0500

    Create tests for components defined as refs twice

---
+++ src/lib/Component.test.ts
@@ -0,0 +1,116 @@
+import dedent from 'ts-dedent';
+import { computed, ref } from '..';
+import { onMounted } from './api/apiLifecycle';
+import { bind } from './bindings/bindingDefinitions';
+import { defineComponent } from './Component';
+import { propType } from './props/propDefinitions';
+import { refComponent } from './refs/refDefinitions';
+
+const mountChild = jest.fn();
+
+describe("Child component defined as 'ref' twice", () => {
+  const ChildComponent = defineComponent({
+    name: 'child-component',
+    props: {
+      onClick: propType.func.shape<(event: MouseEvent) => void>(),
+      text: propType.string.optional,
+      parentAttr: propType.string.optional,
+      grandParentAttr: propType.string.optional,
+    },
+    setup({ refs, props }) {
+      onMounted(mountChild);
+
+      return [
+        bind(refs.self, {
+          click: (event) => props.onClick(event),
+          text: computed(() => props.text),
+          attr: {
+            parent: computed(() => props.parentAttr),
+            grandparent: computed(() => props.grandParentAttr),
+          },
+        }),
+      ];
+    },
+  });
+
+  const handleChildClickFromParent = jest.fn();
+
+  const ParentComponent = defineComponent({
+    refs: {
+      child: refComponent(ChildComponent),
+    },
+    name: 'parent-component',
+    setup({ refs }) {
+      const childText = ref('text from parent');
+      const childAttribute = ref('attr from parent');
+
+      return [
+        bind(refs.child, {
+          onClick: handleChildClickFromParent,
+          text: childText,
+          parentAttr: childAttribute,
+        }),
+      ];
+    },
+  });
+
+  const handleChildClickFromGrandParent = jest.fn();
+
+  const grandParentElement = document.createElement('div');
+  grandParentElement.setAttribute('data-component', 'grandparent-component');
+  grandParentElement.innerHTML = dedent`
+  <div data-component="parent-component">
+    <button data-component="child-component"></button>
+  </div>
+`;
+  document.body.appendChild(grandParentElement);
+
+  const GrandParentComponent = defineComponent({
+    name: 'grandparent-component',
+    refs: {
+      parent: refComponent(ParentComponent),
+      child: refComponent(ChildComponent, {
+        ignoreGuard: true,
+      }),
+    },
+    setup({ refs }) {
+      const childText = ref('text from grandparent');
+      const childAttribute = ref('attr from grandparent');
+
+      return [
+        bind(refs.child, {
+          onClick: handleChildClickFromGrandParent,
+          text: childText,
+          grandParentAttr: childAttribute,
+        }),
+      ];
+    },
+  })(grandParentElement);
+
+  it('should trigger child events in both the parent and grandParent components', async () => {
+    const child = GrandParentComponent.element.querySelector<HTMLButtonElement>(
+      '[data-component=child-component]',
+    );
+
+    if (child) {
+      child.click();
+      const handleChildClickFromGrandParentOrder =
+        handleChildClickFromGrandParent.mock.invocationCallOrder[0];
+      const handleChildClickFromParentOrder =
+        handleChildClickFromParent.mock.invocationCallOrder[0];
+
+      // The component should be mounted two times, one for Parent one for GrandParent
+      expect(mountChild).toHaveBeenCalledTimes(2);
+      // The click function from the parent gets called first, then the grapndparent's
+      expect(handleChildClickFromParentOrder).toBeLessThan(handleChildClickFromGrandParentOrder);
+      expect(handleChildClickFromGrandParent).toHaveBeenCalledTimes(1);
+      expect(handleChildClickFromParent).toHaveBeenCalledTimes(1);
+      // bindings are applied from the child component like Child > Parent > Grandparent
+      // The final textContent should be the one defined in the grandparent
+      expect(child.textContent).toBe('text from grandparent');
+      expect(child.getAttribute('grandparent')).toBe('attr from grandparent');
+      // The grandparent bindings for ChildComponent overwrite the ones declared in the parent component
+      expect(child.getAttribute('parent')).toBe(null);
+    }
+  });
+});
