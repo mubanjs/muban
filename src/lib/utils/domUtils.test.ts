fa76fa5362eb2754dd19273e51b71f6f0d173ffc
Juan Polanco <jspolancor@gmail.com>
2023-05-16 10:00:27 -0500

    Add getOwnerComponent() tests for refs that belong to a wrapper

--- src/lib/utils/domUtils.test.ts
+++ src/lib/utils/domUtils.test.ts
@@ -18,4 +18,24 @@ describe('getOwnerComponent', () => {
     expect(parent).not.toBeNull();
     expect(parent?.getAttribute('data-component')).toBe('my-component');
   });
+  it('should recursively look for parent components and stop if there is a wrapper component without the boundary', () => {
+    const element = document.createElement('div');
+    element.dataset.component = 'my-component';
+    element.innerHTML = `
+      <div data-component="some-wrapper">
+        <button data-ref="toggle">Toggle Content</button>
+        <div data-ref="toggle-content" data-wrapper-boundary>
+          <span data-ref="foo">label</span>
+        </div>
+      </div>
+    `;
+    const fooRef = element.querySelector('[data-ref=foo]')!;
+    const toggleRef = element.querySelector('[data-ref=foo]')!;
+    const fooRefParent = getOwnerComponent(fooRef);
+    const toggleRefParent = getOwnerComponent(toggleRef);
+    expect(fooRefParent).not.toBeNull();
+    expect(toggleRefParent).not.toBeNull();
+    expect(fooRefParent?.getAttribute('data-component')).toBe('some-wrapper');
+    expect(toggleRefParent?.getAttribute('data-component')).toBe('some-wrapper');
+  });
 });
